48148231bae6a21a5fe007d2996a86b9f1e882d1
Kevin de Lange <k.delange@developers.nl>
2020-07-15 09:19:51 +0200

    add local normalize, reduce loaded google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,8 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:ital,wght@0,400;0,500;0,700;1,500&display=swap"
+						href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:ital,wght@0,400;0,500;1,500&display=swap"
 						rel="stylesheet"
 					/>
 					<meta
